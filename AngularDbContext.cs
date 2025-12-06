2a3acaa21e8f9d304c88934a6cac07efa35347dd
KatherineVargasM <katherinevm27@uniandes.edu.ec>
2025-12-06 11:20:20 -0500

    Agregar archivos de proyecto.

---
+++ AngularDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiAngular.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiAngular
+{
+    public class AngularDbContext : DbContext
+    {
+        public AngularDbContext(DbContextOptions op) : base(op)
+        {
+
+        }
+        public DbSet<paisModel> Paises { get; set; }
+        public DbSet<provinciaModel> Provincias { get; set; }
+        public DbSet<cantonModel> Cantones { get; set; }
+        public DbSet<direccionModel> Direcciones { get; set; }
+
+
+    }
+}
